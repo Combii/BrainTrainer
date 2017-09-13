d63858ac881df242f2fbfaf464462eb59a6e7421
Combii <davidstovle@gmail.com>
2017-09-13 16:47:15 +0200

    New working stuff

--- app/src/main/java/com/combii/braintrainer/MainActivity.java
+++ app/src/main/java/com/combii/braintrainer/MainActivity.java
@@ -1,15 +1,96 @@
 package com.combii.braintrainer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    List<TextView> textViewList = new ArrayList<>();
+    TextView timerTextView;
+    TextView scoreTextView;
+    TextView calculationTextView;
+    TextView titleTextView;
+
+    Button button;
+
+
+    int number1, number2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewList.add((TextView) findViewById(R.id.textView));
+        textViewList.add((TextView) findViewById(R.id.textView2));
+        textViewList.add((TextView) findViewById(R.id.textView3));
+        textViewList.add((TextView) findViewById(R.id.textView4));
+
+        timerTextView = (TextView) findViewById(R.id.timerTextView);
+        scoreTextView = (TextView) findViewById(R.id.scoreTextView);
+        calculationTextView = (TextView) findViewById(R.id.calculationTextView);
+        titleTextView = (TextView) findViewById(R.id.titleTextView);
+        button = (Button) findViewById(R.id.button);
+
+
+        setupList();
+        setUpCountDownTimer();
+    }
+
+    public void clickedButton(View view) {
     }
 
 
+    private void generateCalculation(){
+
+    }
+
+
+    private void setupList(){
+        for(final TextView textView : textViewList){
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    checkAnswer(Integer.parseInt(textView.getText().toString()));
+                }
+            });
+        }
+    }
+
+    private void checkAnswer(int value){
+        Log.i("Answer: ", Integer.toString(value));
+    }
+
+    private void setUpCountDownTimer(){
+        new CountDownTimer(30000, 1000) {
+            @Override
+            public void onTick(long l) {
+                updateCountDownTimer((int) l / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+            }
+        }.start();
+    }
+
+    public void updateCountDownTimer(int amount){
+        int minutes = amount / 60;
+        int seconds = amount - minutes * 60;
+
+        String secondsString = Integer.toString(seconds);
+
+        timerTextView.setText(secondsString + 's');
+    }
 }
